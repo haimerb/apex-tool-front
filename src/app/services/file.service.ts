5bc0542170da8fd9849c43c93885770d3313681b
Haymer Barbeti <hbarbeti@in-ui.com>
2024-06-19 20:12:14 -0500

    change this file for generate files and download api services files

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' }),
+  params:new HttpParams()
+};
 const API_URL = 'http://localhost:8080/api/test/';
 
 @Injectable({
@@ -9,6 +14,31 @@ const API_URL = 'http://localhost:8080/api/test/';
 })
 export class FIleService {
   constructor(private http: HttpClient) {}
+
+
+
+  upLoadFIle(fileUpload:FormData ): Observable<any> {
+    httpOptions.headers.append("accept","application/json");
+    return this.http.post(
+      environment.apiUrl+'files.php/files',
+      {
+        fileUpload
+      },
+      httpOptions
+    );
+  }
+
+  downloadFIle(nameFile:any ): Observable<any> {
+    //httpOptions.headers.append("accept", "*/*");
+    //httpOptions.headers.append("accept","gzip, deflate, br");
+    const options = {
+      params:new HttpParams().set("file",nameFile)
+    };
+  return this.http.get(
+      environment.apiUrl+'files.php/files/downloadFile',
+      options
+    );
+  }
 
   getPublicContent(): Observable<any> {
     return this.http.get(API_URL + 'all', { responseType: 'text' });
@@ -26,3 +56,6 @@ export class FIleService {
     return this.http.get(API_URL + 'admin', { responseType: 'text' });
   }
 }
+
+
+
