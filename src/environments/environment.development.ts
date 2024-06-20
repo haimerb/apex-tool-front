62d846b4cf53b7f220575642784025e76a3a2258
Haymer Barbeti <hbarbeti@in-ui.com>
2024-06-19 20:12:14 -0500

    change this file for set environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/',
-  assertsPath:'./assets/'
+  assertsPath:'./assets/',
+  downloadPathApi:'http://localhost:8000/api/tmp/mpdf/outfiles/'
 };
