11691a077b290a11fc3d1cc6aa3abf141deec486
Haymer Barbeti <hbarbeti@in-ui.com>
2024-07-06 19:01:08 -0500

    change file modified:   src/app/app-routing.module.ts for optimizations in read files from frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { Landing } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
