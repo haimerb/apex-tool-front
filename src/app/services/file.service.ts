1d1687b96b3d4a1bc5a915dece043399e4291464
Haymer Barbeti <hbarbeti@in-ui.com>
2024-07-06 19:01:11 -0500

    change file  modified:   src/app/services/file.service.ts for optimizations in read files from frontend

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -16,7 +16,7 @@ export class FIleService {
   constructor(private http: HttpClient) {}
   upLoadFIle(upLoadFIle:FormData): Observable<any> {
 
-    console.log(upLoadFIle,"Antes-21");
+    //console.log(upLoadFIle,"Antes-21");
     // let data=new Blob(fileUpload);
     //let file=upLoadFIle[0];
     //let fd=new FormData();
@@ -33,7 +33,8 @@ export class FIleService {
     h.headers.append("Content-Disposition", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     //  httpOptions.headers.append('Content-Encoding', ' gzip, deflate, br, zstd');
     //  httpOptions.headers.append('Accept-Encoding', 'gzip, deflate, br');
-    //console.log(fd.getAll("upLoadFIle")[0],"upLoadFIle");
+    ////console.log(fd.getAll("upLoadFIle")[0],"upLoadFIle");
+    h.headers.append("accept", "application/json");
     return this.http.post(environment.apiUrl+'files.php/files',
       upLoadFIle,
       {
@@ -53,18 +54,22 @@ export class FIleService {
     );
   }
 
-  // setCertificate(nit:string,tipo_retencion:string,year_tribute:string,idOrganizacion:string): Observable<any> {
 
-  //   return this.http.post(
-  //     environment.apiUrl+'files.php/files/base',
-  //     {
-  //       nit,
-  //       tipo_retencion,
-  //       year_tribute,
-  //       idOrganizacion
-  //     }
-  //   );
-  // }
+  readFIle(nameFile:any): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({ 'Content-Type': 'text/plain; charset=utf-8' })
+    };
+    //options.headers.append("accept", "*/*");
+    //options.headers.append("Accept-Encoding","gzip, deflate, br");
+    options.headers.append("Accept","application/json; charset=utf-8");
+    return this.http.post(environment.apiUrl+'api.php/files/procesFile',
+      {
+        nameFile
+      },
+      options
+    );
+  }
+
 }
 
 
