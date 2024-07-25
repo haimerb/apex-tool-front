bc2db71a820ecaed1eab0a14718b381cf3c10bdf
Haymer Barbeti <hbarbeti@in-ui.com>
2024-07-25 18:54:22 -0500

    end dev solution for qa test

---
+++ src/app/models/certificatePreGenerate.model.ts
@@ -0,0 +1,9 @@
+export class CertificatePreGenerateItem {
+  nit?: string;
+  dataType?:number;
+  year_tribute?: string;
+  cuantos?:number;
+  range_ini?:string;
+  range_end?:string;
+  city?:string;
+}
---
+++ src/app/models/tutorial.model copy.ts
@@ -0,0 +1,9 @@
+export class RowItemSave{
+  code?:number;
+  id_certificate_data?:string;
+  nit?:string;
+  nombreConcepto?:string;
+  razonSocial?:string;
+
+
+}
